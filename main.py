135390c79a6708e51390f7e4dbc8ddf795dc58b7
k-d-yu <dima_kul95@mail.ru>
2022-12-06 20:06:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# class Point:
+#     """Класс для представления координат точек на плоскости"""
+#     x = 1
+#     y = 1
+#
+#
+# # print(Point.__doc__)
+# # print(dir(Point))
+#
+# p1 = Point()
+# p2 = Point()
+# print("p1 = ", p1.x)
+# print("p2 = ", p2.x)
+# print("Point = ", Point.x)
+# # print(type(p1))
+# p1.x = 100
+# p2.x = 200
+# print("p1 = ", p1.x)
+# print("p2 = ", p2.x)
+# print("Point = ", Point.x)
+# print(id(p1))
+# print(id(p2))
+# print(id(Point))
+# Point.y = 300
+# print("p1 = ", p1.y)
+# print("p2 = ", p2.y)
+# print("Point = ", Point.y)
+
+# class Point:
+#     """Класс для представления координат точек на плоскости"""
+#     x = 1
+#     y = 1
+#
+#
+# p1 = Point()
+# p1.x = 5
+# p1.y = 10
+# p1.z = 20
+# print(p1.x, p1.y)
+# print(p1.__dict__)
+# print(Point.__dict__)
+
+
+class Point:
+    """Класс для представления координат точек на плоскости"""
+    x = 1
+    y = 1
+
+
